1b8852b698fee52fdbacc357e2a04c67ac73488c
Dmitrii Oznobikhin <doznobikhin@gmail.com>
2020-03-04 22:49:10 +0300

    complete code

--- src/com/sbt/jscool/tasks/_2005/Solution.java
+++ src/com/sbt/jscool/tasks/_2005/Solution.java
@@ -5,6 +5,10 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+/*
+добавил текстовый комментарий
+ */
+
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
