2abde9b3a7a4e71d76667a1ba20292b4fb947666
Dmitrii Oznobikhin <doznobikhin@gmail.com>
2020-03-04 22:38:15 +0300

    add task solution

---
+++ src/com/sbt/jscool/tasks/_2005/Solution.java
@@ -0,0 +1,32 @@
+package com.sbt.jscool.tasks._2005;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(reader.readLine());
+        StringTokenizer st = new StringTokenizer(reader.readLine());
+
+        int[] array = new int[n];
+
+        for (int i = 0; st.hasMoreTokens() && i < n; i++) {
+            array[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int min = array[0];
+        int minIndex = 0;
+        for (int j = 1; j < array.length; j++) {
+            if (array[j] < min) {
+                min = array[j];
+                minIndex = j;
+            }
+        }
+
+        System.out.println(minIndex + 1);
+    }
+}
+
