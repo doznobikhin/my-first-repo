5be389b9bf3f0bf9219b58f17f74aadc681aa332
Dmitrii Oznobikhin <doznobikhin@gmail.com>
2020-03-11 18:12:56 +0300

    Полное решение

--- src/com/sbt/jscool/tasks/_2024/Solution.java
+++ src/com/sbt/jscool/tasks/_2024/Solution.java
@@ -3,9 +3,6 @@ package com.sbt.jscool.tasks._2024;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Solution {
@@ -15,29 +12,16 @@ public class Solution {
         StringTokenizer st = new StringTokenizer(reader.readLine());
 
         int[] array = new int[n];
-
         for (int i = 0; st.hasMoreTokens() && i < n; i++) {
             array[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(array);
 
-        System.out.println(Arrays.toString(array));
-
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < array.length - (array.length % 2); i++) {
-            if (map.containsKey(array[i])) {
-                int value = map.get(array[i]);
-                map.replace(array[i], value + 1);
-            } else {
-                map.put(array[i], 1);
+        int number = 0;
+        for (int i = 0; i < array.length / 2; i++) {
+            if (array[i] != array[array.length - 1 - i]) {
+                number++;
             }
         }
-
-        int sum = 0;
-        for (Map.Entry<Integer, Integer> pair : map.entrySet()) {
-            sum += pair.getValue() % 2;
-        }
-        System.out.println(sum / 2);
+        System.out.println(number);
     }
-
 }
