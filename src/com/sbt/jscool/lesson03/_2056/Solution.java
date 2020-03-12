59697e03ea9e1c8fadff9dcc584fc44027bd25d2
Dmitrii Oznobikhin <doznobikhin@gmail.com>
2020-03-12 11:49:29 +0300

    Полное решение

--- src/com/sbt/jscool/lesson03/_2056/Solution.java
+++ src/com/sbt/jscool/lesson03/_2056/Solution.java
@@ -1,41 +1,53 @@
 package com.sbt.jscool.lesson03._2056;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Solution {
-    public static void main(String[] args) {
-        //String input = "a aa aaa aaaa\na AA aaa bbbb";
-        String input = "Sebastian Vettel is a German Formula One racing driver currently driving for Red Bull Racing";
-        /*System.out.println(input);
-        System.out.println();*/
-        List<String> list = parseString(input.toLowerCase());
-        /*for (String str : list) {
-            System.out.println(str);
-        }*/
+    public static void main(String[] args) throws IOException {
+        /*BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String input = reader.readLine();
+        input = input.toLowerCase().replaceAll("\\s{1,}"," ").trim();*/
 
-        Set<String> set = new TreeSet<>(list);
-        Map<String,Integer> map = new TreeMap<>();
+        Path inputFile = Paths.get("input.txt");
+        List<String> list = Files.readAllLines(inputFile);
+        StringBuilder sb = new StringBuilder();
+        for (String str : list) {
+            sb.append(str);
+            sb.append("\n");
+        }
+
+        String input = sb.toString().toLowerCase().replaceAll("\\s{1,}", " ").trim();
+
+        List<String> wordsList = new ArrayList<>(Arrays.asList(input.split(" ")));
+        //wordsList.addAll(Arrays.asList(input.split(" ")));
+
+        Set<String> set = new TreeSet<>(wordsList);
+        Map<String, Integer> map = new TreeMap<>();
+
         int max = 1;
         for (String str : set) {
-            int frequency = Collections.frequency(list,str);
-            //System.out.println(str + " " + frequency);
-            map.put(str,frequency);
+            int frequency = Collections.frequency(wordsList, str);
+            map.put(str, frequency);
             if (frequency > max) {
                 max = frequency;
             }
         }
         for (Map.Entry<String, Integer> pair : map.entrySet()) {
             if (max == pair.getValue()) {
-                System.out.printf("%s%n",pair.getKey());
+                System.out.printf("%s%n", pair.getKey());
             }
         }
     }
 
     static List<String> parseString(String s) {
         List<String> list = new ArrayList<>();
-        Pattern pattern = Pattern.compile("[a-zA-Zа-яА-Я]+");
+        Pattern pattern = Pattern.compile("[a-zA-Z]+");
         Matcher matcher = pattern.matcher(s);
         while (matcher.find()) {
             list.add(s.substring(matcher.start(), matcher.end()));
