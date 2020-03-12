d99b5caed459d801d0c68e2442e802ba5b7cd757
Dmitrii Oznobikhin <doznobikhin@gmail.com>
2020-03-13 01:40:05 +0300

    Частичное решение 2034

--- src/com/sbt/jscool/tasks/_2034/Solution.java
+++ src/com/sbt/jscool/tasks/_2034/Solution.java
@@ -10,39 +10,39 @@ public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(reader.readLine());
+        //1 ≤ n ≤ 10000
+        if (n < 1 || n > 10001) {
+            System.exit(1);
+        }
         StringTokenizer st = new StringTokenizer(reader.readLine());
-
         int[] array = new int[n];
-
         for (int i = 0; st.hasMoreTokens() && i < n; i++) {
             array[i] = Integer.parseInt(st.nextToken());
         }
 
+        //System.out.println(Arrays.toString(array));
+
         cycle:
-        for (int i = array.length; i >= 2; i--) {
+        for (int i = array.length; i >= 1; i--) {
             for (int j = 0; j <= (array.length - i); j++) {
                 int[] tmpArray = Arrays.copyOfRange(array, j, j + i);
                 if (isCons(tmpArray)) {
                     System.out.println((j + 1) + " " + (i + j));
                     break cycle;
                 }
+                //System.out.println(Arrays.toString(tmpArray) + " " + isCons(tmpArray));
             }
         }
 
     }
 
     private static boolean isCons(int[] array) {
-        int min = array[0];
-        int max = array[0];
-        for (int i = 1; i < array.length; i++) {
-            if (array[i] > max) {
-                max = array[i];
-            }
-            if (array[i] < min) {
-                min = array[i];
-            }
-        }
-        return max - min <= 1;
+        /*int[] copiedArray = Arrays.copyOf(array, array.length);
+        Arrays.sort(copiedArray);
+        return copiedArray[copiedArray.length - 1] - copiedArray[0] <= 1;
+        */
+        Arrays.sort(array);
+        return array[array.length - 1] - array[0] <= 1;
     }
 }
 
