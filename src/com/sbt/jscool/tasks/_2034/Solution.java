777d2bc7d14a57d3276b1704bb114f48acc30829
Dmitrii Oznobikhin <doznobikhin@gmail.com>
2020-03-14 01:42:12 +0300

    Частичное решение 2034

--- src/com/sbt/jscool/tasks/_2034/Solution.java
+++ src/com/sbt/jscool/tasks/_2034/Solution.java
@@ -3,10 +3,20 @@ package com.sbt.jscool.tasks._2034;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Solution {
+    private int[] array;
+    private List<Point> list;
+
+    public Solution(int[] array) {
+        this.array = array;
+        this.list = new ArrayList<>();
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(reader.readLine());
@@ -14,35 +24,90 @@ public class Solution {
         if (n < 1 || n > 10001) {
             System.exit(1);
         }
+
+
         StringTokenizer st = new StringTokenizer(reader.readLine());
         int[] array = new int[n];
         for (int i = 0; st.hasMoreTokens() && i < n; i++) {
             array[i] = Integer.parseInt(st.nextToken());
         }
 
-        //System.out.println(Arrays.toString(array));
 
-        cycle:
-        for (int i = array.length; i >= 1; i--) {
-            for (int j = 0; j <= (array.length - i); j++) {
-                int[] tmpArray = Arrays.copyOfRange(array, j, j + i);
-                if (isCons(tmpArray)) {
-                    System.out.println((j + 1) + " " + (i + j));
-                    break cycle;
-                }
-                //System.out.println(Arrays.toString(tmpArray) + " " + isCons(tmpArray));
-            }
-        }
+        Solution sol = new Solution(array);
+        sol.findConsArray(0, sol.array.length - 1);
+        Collections.sort(sol.list);
+
+        //System.out.println(sol.list);
+        System.out.println(sol.list.get(0));
+
+
+        //     2 3 2 1 2 3 4 2
+        //           1     4        3
+        //     2 3 2 1 2 3            2 3 4 2
+        //       3   1     2          2   4    2
+        //     2 3 2   2 1 2 3        2 3    3 4 2
+        //     2 3       1   3        2 3      4 2
 
     }
 
-    private static boolean isCons(int[] array) {
-        /*int[] copiedArray = Arrays.copyOf(array, array.length);
-        Arrays.sort(copiedArray);
-        return copiedArray[copiedArray.length - 1] - copiedArray[0] <= 1;
-        */
-        Arrays.sort(array);
-        return array[array.length - 1] - array[0] <= 1;
+    public void findConsArray(int begin, int end) {
+        for (Point pt : list) {
+            if (pt.getLenght() > end - begin) {
+                return;
+            }
+        }
+
+        int max = array[begin];
+        int min = array[begin];
+        int indxMax = begin;
+        int indxMin = begin;
+        for (int i = begin; i <= end; i++) {
+            if (array[i] > max) {
+                max = array[i];
+                indxMax = i;
+            } else if (array[i] < min) {
+                min = array[i];
+                indxMin = i;
+            }
+        }
+        if (max - min <= 1) {
+            list.add(new Point(begin, end));
+            return;
+        }
+
+        int start = indxMax;
+        int finish = indxMin;
+        if (indxMax > indxMin) {
+            start = indxMin;
+            finish = indxMax;
+        }
+        findConsArray(begin, finish - 1);
+        findConsArray(start + 1, end);
+    }
+    // begin  start    finish      end
+
+    public static class Point implements Comparable<Point> {
+        private int x;
+        private int y;
+
+        public Point(int x, int y) {
+            this.x = x + 1;
+            this.y = y + 1;
+        }
+
+        public int getLenght() {
+            return y - x;
+        }
+
+        @Override
+        public int compareTo(Point o) {
+            return this.y - this.x == o.y - o.x ? this.x - o.x : (o.y - o.x) - (this.y - this.x);
+        }
+
+        @Override
+        public String toString() {
+            return x + " " + y;
+        }
     }
 }
 
